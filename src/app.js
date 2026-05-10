0a9a21f5f13924e82abe12e4033a2e6bea009648
Edivan da Silva Junior <edivanjr13@gmail.com>
2026-05-10 14:44:20 -0300

    feat: add initial files and setup static file serving

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const router = require('./routes/index')
 
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
 
 app.use(router);
