ef350465205e5a189095c6480bbe9d0e359cb088
Edivan da Silva Junior <edivanjr13@gmail.com>
2026-05-11 20:01:23 -0300

    feat: adicionar rota de registro e criar formulário para entrada de dados para fins de teste

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const router = require('./routes/index')
+const router = require('./routes/index');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 app.use(router);
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,13 @@ const exampleRouter2 = require("../modules/example2/example2.routes");
 const router = Router();
 
 router.get('/', (req, res) => res.render('index'));
+router.get('/form', (req, res) => res.render('form'));
+
+router.post('/register', (req, res) => {
+    const { username } = req.body;
+    console.log(username);
+    res.redirect('/form');
+});
 
 router.use('/example', exampleRouter);
 router.use('/example2', exampleRouter2);
